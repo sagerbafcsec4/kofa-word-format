0dff87774e2217988f1d791fe5fe61f54cbea1c8
sagerbafcsec4 <62989854+sagerbafcsec4@users.noreply.github.com>
2026-06-23 15:58:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taisen-format-v30';
+const CACHE = 'taisen-format-v31';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
