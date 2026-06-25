32cc4f291224436895f5ed80b113e15f60ce4091
sagerbafcsec4 <62989854+sagerbafcsec4@users.noreply.github.com>
2026-06-25 15:14:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taisen-format-v46';
+const CACHE = 'taisen-format-v47';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./favicon-16.png','./favicon-32.png'];
 
 self.addEventListener('install', e => {
